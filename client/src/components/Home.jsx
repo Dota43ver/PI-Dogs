d15b11a9aec6c1d4d1776f14e4bd994d3d49adb1
Facundo aguero <facundoagueroc@gmail.com>
2022-11-24 19:07:42 -0300

    some uptdates

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {useState, useEffect} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
-import { getDogs,filterDogsByTemperament, filterCreated, orderByName, orderByWeight } from '../actions';
+import { getDogs,filterDogsByTemperament, filterCreated, orderByName, orderByWeight, getTemperaments } from '../actions';
 import {Link} from 'react-router-dom';
 import Card from './Card';
 import Pagination from './Pagination';
@@ -12,9 +12,11 @@ export default function Home(){
 
 const dispatch = useDispatch()
 const allDogs = useSelector((state) => state.dogs)
+const temperaments = useSelector((state) => state.temperaments)
 const [orden, setOrden] = useState('')
 const [currentPage,setCurrentPage] = useState(1)
 const [dogsPerPage, setDogsPerPage] = useState(8)
+const [refresh, setRefresh] = useState()
 const indexOfLastDog = currentPage * dogsPerPage
 const indexOfFirstDog = indexOfLastDog - dogsPerPage
 const currentDogs = allDogs.slice(indexOfFirstDog,indexOfLastDog)
@@ -25,29 +27,40 @@ const pagination = (pageNumber) => {
 
 useEffect(()=>{
      dispatch(getDogs());
+     dispatch(getTemperaments())
 },[dispatch])
 
 function handleClick(e){
 e.preventDefault();
 dispatch(getDogs());
+setRefresh('default')
+setCurrentPage(1)
 }
 
 function handleFilterTemperament(e){
 dispatch(filterDogsByTemperament(e.target.value))
+setCurrentPage(1)
+setRefresh()
 }
 
 function handleFilterCreated(e){
     dispatch(filterCreated(e.target.value))
+    setCurrentPage(1)
+    setRefresh()
 }
 const handleOrderByName = (e) => {
     e.preventDefault();
     dispatch(orderByName(e.target.value));
+    setCurrentPage(1)
     setOrden(`Ordenado ${e.target.value}`);
+    setRefresh()
   };
   const handleOrderByWeight = (e) => {
     e.preventDefault();
     dispatch(orderByWeight(e.target.value));
+    setCurrentPage(1)
     setOrden(`Ordenado ${e.target.value}`);
+    setRefresh()
   };
 
 return(
@@ -58,150 +71,29 @@ return(
             volver a cargar dogs
         </button>
         <div>
-        <select onChange={handleOrderByName} className={style.select_temperaments}>
-                <option disabled selected defaultValue>
+        <select onChange={handleOrderByName} className={style.select_temperaments} value={refresh}>
+                <option value="default" disabled selected defaultValue >
                   Alphabetical order
                 </option>
                 <option value="asc">asc</option>
                 <option value="desc">desc</option>
               </select>
-        <select onChange={handleOrderByWeight} className={style.select_temperaments}>
-            <option disabled selected defaultValue>
+        <select onChange={handleOrderByWeight} className={style.select_temperaments} value={refresh}>
+            <option value='default' disabled selected defaultValue>
                 less weight order
             </option>
             <option value="asc">asc</option>
             <option value="desc">desc</option>
         </select>
-            <select onChange={e => handleFilterTemperament(e)} className={style.select_temperaments}>
-                <option value="All">All</option>
-                <option value="Confident">Confident</option>
-                <option value="Trainable">Trainable</option>
-                <option value="Fast">Fast</option>
-                <option value="Stubborn">Stubborn</option>
-                <option value="Active">Active</option>
-                <option value="Agile">Agile</option>
-                <option value="Bright">Bright</option>
-                <option value="Joyful">Joyful</option>
-                <option value="Adaptable">Adaptable</option>
-                <option value="Spirited">Spirited</option>
-                <option value="Spunky">Spunky</option>
-                <option value="Unflappable">Unflappable</option>
-                <option value="Gay">Gay</option>
-                <option value="Patient">Patient</option>
-                <option value="Easygoing">Easygoing</option>
-                <option value="Lively">Lively</option>
-                <option value="Self-assured">Self-assured</option>
-                <option value="Sociable">Sociable</option>
-                <option value="Diligent">Diligent</option>
-                <option value="Good-natured">Good-natured</option>
-                <option value="Trusting">Trusting</option>
-                <option value="Fierce">Fierce</option>
-                <option value="Hardworking">Hardworking</option>
-                <option value="Cautious">Cautious</option>
-                <option value="Benevolent">Benevolent</option>
-                <option value="Reliable">Reliable</option>
-                <option value="Mischievous">Mischievous</option>
-                <option value="Bubbly">Bubbly</option>
-                <option value="Vigilant">Vigilant</option>
-                <option value="Cooperative">Cooperative</option>
-                <option value="Loving">Loving</option>
-                <option value="Brave">Brave</option>
-                <option value="Cheerful">Cheerful</option>
-                <option value="Bold">Bold</option>
-                <option value="Vocal">Vocal</option>
-                <option value="Respectful">Respectful</option>
-                <option value="Watchful">Watchful</option>
-                <option value="Lovable">Lovable</option>
-                <option value="Strong">Strong</option>
-                <option value="Fun-loving">Fun-loving</option>
-                <option value="Even Tempered">Even Tempered</option>
-                <option value="Energetic">Energetic</option>
-                <option value="Sensitive">Sensitive</option>
-                <option value="Self-important">Self-important</option>
-                <option value="Familial">Familial</option>
-                <option value="Extroverted">Extroverted</option>
-                <option value="Responsible">Responsible</option>
-                <option value="Docile">Docile</option>
-                <option value="Quiet">Quiet</option>
-                <option value="Alert">Alert</option>
-                <option value="Sturdy">Sturdy</option>
-                <option value="Clownish">Clownish</option>
-                <option value="Reserved">Reserved</option>
-                <option value="Cat-like">Cat-like</option>
-                <option value="Receptive">Receptive</option>
-                <option value="Kind">Kind</option>
-                <option value="Opinionated">Opinionated</option>
-                <option value="Proud">Proud</option>
-                <option value="Obedient">Obedient</option>
-                <option value="People-Oriented">People-Oriented</option>
-                <option value="Thoughtful">Thoughtful</option>
-                <option value="Strong Willed">Strong Willed</option>
-                <option value="Tolerant">Tolerant</option>
-                <option value="Steady">Steady</option>
-                <option value="Stable">Stable</option>
-                <option value="Companionable">Companionable</option>
-                <option value="Wild">Wild</option>
-                <option value="Territorial">Territorial</option>
-                <option value="Tenacious">Tenacious</option>
-                <option value="Fearless">Fearless</option>
-                <option value="Hard-working">Hard-working</option>
-                <option value="Powerful">Powerful</option>
-                <option value="Intelligent">Intelligent</option>
-                <option value="Inquisitive">Inquisitive</option>
-                <option value="Independent">Independent</option>
-                <option value="Clever">Clever</option>
-                <option value="Sweet-Tempered">Sweet-Tempered</option>
-                <option value="Suspicious">Suspicious</option>
-                <option value="Rugged">Rugged</option>
-                <option value="Composed">Composed</option>
-                <option value="Aggressive">Aggressive</option>
-                <option value="Friendly">Friendly</option>
-                <option value="Dutiful">Dutiful</option>
-                <option value="Rational">Rational</option>
-                <option value="Charming">Charming</option>
-                <option value="Keen">Keen</option>
-                <option value="Good-tempered">Good-tempered</option>
-                <option value="Gentle">"Gentle"</option>
-                <option value="Quick">Quick</option>
-                <option value="Devoted">Devoted</option>
-                <option value="Amiable">Amiable</option>
-                <option value="Loyal">Loyal</option>
-                <option value="Dominant">Dominant</option>
-                <option value="Willful">Willful</option>
-                <option value="Reserved">Reserved</option>
-                <option value="Refined">Refined</option>
-                <option value="Feisty">Feisty</option>
-                <option value="Outgoing">Outgoing</option>
-                <option value="Responsive">Responsive</option>
-                <option value="Bossy">Bossy</option>
-                <option value="Determined">Determined</option>
-                <option value="Trustworthy">Trustworthy</option>
-                <option value="Attentive">Attentive</option>
-                <option value="Great-hearted">Great-hearted</option>
-                <option value="Assertive">Assertive</option>
-                <option value="Protective">Protective</option>
-                <option value="Hardy">Hardy</option>
-                <option value="Playful">Playful</option>
-                <option value="Happy">Happy</option>
-                <option value="Cunning">Cunning</option>
-                <option value="Aloof">Aloof</option>
-                <option value="Excitable">Excitable</option>
-                <option value="Courageous">Courageous</option>
-                <option value="Affectionate">Affectionate</option>
-                <option value="Self-confidence">Self-confidence</option>
-                <option value="Curious">Curious</option>
-                <option value="Dignified">Dignified</option>
-                <option value="Calm">Calm</option>
-                <option value="Generous">Generous</option>
-                <option value="Athletic">Athletic</option>
-                <option value="Faithful">Faithful</option>
-                <option value="Adventurous">Adventurous</option>
-                <option value="Merry">Merry</option>
-                <option value="Boisterous">Boisterous</option>
-                <option value="Eager">Eager</option>
+            <select onChange={e => handleFilterTemperament(e)} className={style.select_temperaments} value={refresh}>
+                <option value='default' disabled selected defaultValue>temperaments</option>
+            {temperaments.map((e)=>(
+                    <option value={e.name} key={e.id} className={style.option_temperaments}>{e.name}</option>
+                ))}
+                
             </select>
-            <select onChange={e => handleFilterCreated(e)} className={style.select_temperaments}>
-                <option value="All">All</option>
+            <select onChange={e => handleFilterCreated(e)} className={style.select_temperaments} value={refresh}>
+                <option value="default" disabled selected defaultValue>All</option>
                 <option value="created">created dogs</option>
                 <option value="api">api Dogs</option>
             </select>
@@ -211,7 +103,7 @@ return(
             allDogs={allDogs.length}
             pagination= {pagination}/>
             </div>
-            <SearchBar/>
+            <SearchBar pagination={pagination}/>
             <div className={style.main_container}>
                 <div className={style.container_cards}>
             {
--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import {useState} from "react";
 import {useDispatch} from "react-redux";
 import { getNameDogs } from "../actions";
+import { useHistory } from "react-router-dom";
 import style from "./SearchBar.module.css"
 
-export default function SearchBar(){
+export default function SearchBar({pagination}){
 const dispatch = useDispatch()
 const [name, setName] = useState("")
+const history = useHistory();
 
 function handleInputChange(e){
 e.preventDefault()
@@ -17,6 +19,9 @@ console.log(name)
 function handleSubmit(e){
 e.preventDefault()
 dispatch(getNameDogs(name))
+setName("")
+history.push('/home')
+pagination(1)
 }
 return (
     <div>
