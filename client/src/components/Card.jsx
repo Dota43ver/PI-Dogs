9fa39b6152d08da713cfb4f8386cddeba74cff9c
Facundo aguero <facundoagueroc@gmail.com>
2022-12-02 17:33:38 -0300

    styles

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import style from "./Home.module.css"
 
 
 export default function Card({ name, image, temperament, weight}){
@@ -11,7 +12,7 @@ export default function Card({ name, image, temperament, weight}){
             {temperament + ", "}
             </div>
             <h5>{weight + " kg"}</h5>
-            <img src={image} alt="sin imagen" width="200px" height="250px" />
+            <img className={`${style.img_card}`} src={image} alt="sin imagen" width="300px" height="300px" />
         </div>
     )
 }
